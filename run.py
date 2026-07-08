f202a5ceedc2b12b41a60207865647c60994e8a9
Matheus Machado <matheusmazar@gmail.com>
2026-07-08 17:18:11 +0000

    auto find where i am

--- run.py
+++ run.py
@@ -3,7 +3,33 @@ from src import energyuq
 from src import programs
 from src import machines
 
-campaign, analysis = energyuq.create(programs.HPCG, machines.Glados)
+from socket import gethostname
+
+import re
+import sys
+
+#todo: more command line parameters
+
+
+# todo: extract details programatically. Remove this horror
+POSSIBILITIES: list[tuple[re.Pattern, type[machines.Machine]]] = [
+    (re.compile(r"glados", re.IGNORECASE), machines.Glados),
+    (re.compile(r"hype\d", re.IGNORECASE), machines.Hype),
+]
+
+host = sys.argv[1] if len(sys.argv) > 1 else gethostname()
+
+mach = None
+
+for pat, m in POSSIBILITIES:
+    if pat.match(host):
+        mach = m
+        break
+
+if mach is None:
+    raise RuntimeError("I don't know where I am at")
+
+campaign, analysis = energyuq.create(programs.HPCG, mach)
 
 energyuq.refine_and_analyse(campaign, analysis, max_number_of_refinements=100)
 
